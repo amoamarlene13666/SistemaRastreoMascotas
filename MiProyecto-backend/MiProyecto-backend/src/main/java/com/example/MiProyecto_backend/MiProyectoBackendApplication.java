3a7fb4a78ca47748147b602b9029f28ee19a5b55
Andrea Marlene <marlene.ortega@estudiante.uacm.edu.mx>
2025-03-20 19:57:44 -0600

    Añadidos archivos del backend al repositorio

---
+++ MiProyecto-backend/MiProyecto-backend/src/main/java/com/example/MiProyecto_backend/MiProyectoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiProyecto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiProyectoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiProyectoBackendApplication.class, args);
+	}
+
+}
---
+++ MiProyecto-backend/MiProyecto-backend/src/test/java/com/example/MiProyecto_backend/MiProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiProyecto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiProyectoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
